c98971804cfe98c38329fea1ac3ad193c9395dd5
zaid2255 <65081907+zaid2255@users.noreply.github.com>
2022-11-11 05:38:03 +0500

    1st page changes
    1st page changes

--- bus.php
+++ bus.php
@@ -39,8 +39,9 @@
             <div class="col-sm-12 col-lg-12">
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
-                        <div class="header-title">
+                        <div class="header-title w-100">
                             <h4 class="card-title">OnLine Bus Booking</h4>
+                            <hr>
                         </div>
                     </div>
                     <div class="card-body p-0">
@@ -48,31 +49,49 @@
                             <div class="bussw_inner row">
                                 <div class="bussw_inputBox selectHtlCity  ">
                                     <label for="fromCity" class="lbl_input makeFlex column latoBold"><span data-cy="fromCity" class="appendBottom5">From</span>
-                                        <input id="fromCity" data-cy="fromCityVal" type="text" class="bussw_inputField font30 latoBlack" readonly="" value="Delhi, Delhi">
+                                        
+                                        <select id="fromCity" data-cy="fromCityVal" class="bussw_inputField font30 latoBlack">
+                                            <option>Delhi, Delhi</option>
+                                            <option>Mumbai</option>
+                                            <option>Banglore</option>
+                                            <option>Pune</option>
+                                        </select>
                                         <p data-cy="fromCountryCode" class="code">India</p>
                                     </label>
                                 </div>
                                 <div class="bussw_inputBox selectHtlCity  ">
                                     <label for="toCity" class="lbl_input latoBold makeFlex column"><span data-cy="toCity" class="appendBottom5">To</span>
-                                        <input id="toCity" data-cy="toCityVal" type="text" class="bussw_inputField font30 latoBlack" readonly="" value="Kanpur, Uttar Pradesh">
+                                        <select id="toCity" data-cy="fromCityVal" class="bussw_inputField font30 latoBlack">
+                                            <option>Delhi, Delhi</option>
+                                            <option>Mumbai</option>
+                                            <option>Banglore</option>
+                                            <option>Pune</option>
+                                        </select>
                                         <p data-cy="toCountryCode" class="code">India</p>
                                     </label>
                                 </div>
                                 <div class="bussw_inputBox dayPickerRailWrap dates  ">
                                     <label for="travelDate" class="lbl_input latoBold makeFlex column"><span data-cy="travelDate" class="appendBottom5 downArrow">Travel Date</span>
-                                        <input id="travelDate" type="text" data-cy="travelDateVal" class="bussw_inputField font20" readonly="" value="">
-                                        <p data-cy="departureDate" class="blackText font20 code paddingTB2"><span class="font20 latoBlack ">12</span><span> Nov</span><span class="shortYear">22</span></p>
+                                        <div class="bd-example">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control vanila-datepicker" placeholder="Date" style='opacity: 1;font-size: 18px;font-family: "Lato", sans-serif;'>
+                                            </div>
+                                        </div>
                                         <p data-cy="departureDay" class="code">Saturday</p>
                                     </label>
+
                                 </div>
                             </div>
+
+
                             <p class="makeFlex vrtlCenter">
-                                <div class="form-group">
-                                <div class="d-flex form-check form-check-inline justify-content-center" style="padding-left: 0;">
-                                    <button type="button" class="bg_gradient btn btn_style2">Search</button>
+                            <div class="form-group">
+                                <div class="d-flex form-check form-check-inline justify-content-center btn_new_style" style="padding-left: 0;">
+                                    <button type="button" class="bg_gradient btn btn_style2 new_style">Search</button>
                                 </div>
                             </div>
                             </p>
+                            
                         </div>
                     </div>
                 </div>
